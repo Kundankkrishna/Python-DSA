17339d736752a530735f234a8f86e3839f858215
Kundan Krishna <kundankk1339@gmail.com>
2023-04-11 00:36:03 +0530

    Program to find square root of a number

---
+++ squareroot.py
@@ -0,0 +1,27 @@
+# Calculate the square root of a number
+
+# num = int(input("Enter number:"))
+# def rootFinder(x):
+#     count = 0
+#     for i in range(1,x+1):
+#         if i*i == x:
+#             count +=1
+#             return i
+#     if count == 0:
+#         print("Entry not a perfect square")
+# print(rootFinder(num))
+
+num = int(input("enter number: "))
+
+def rootFinder(x):
+    i =1
+    ans = 1
+    while i*i <= x:
+        ans = i
+        i +=1
+    if ans*ans == x:
+        return ans
+    else:
+        return "Not a perfect square"
+
+print(rootFinder(num))
