1d52671ecad708747db1e8da187a0651e2d3358c
Kundan Krishna <kundankk1339@gmail.com>
2023-06-23 11:43:26 +0530

    enforced PEP8

--- countgreater.py
+++ countgreater.py
@@ -1,19 +1,22 @@
 # count the number of elements having at least 1 element greater than itself
 
 num = int(input("Enter the size of array: "))
-list=[]
-for i in range(0,num):
+A = []
+for i in range(0, num):
     elements = int(input("Enter element: "))
-    list.append(elements)
+    A.append(elements)
+
 
 def countGreater(array):
-    max = array[0]
+    maxim = array[0]
     frequency = 0
-    for i in range(0,len(array)):
-        if array[i] == max:
+    for i in range(0, len(array)):
+        if array[i] == maxim:
             frequency += 1
-        if array[i] > max:
-            max = array[i]
-            frequency =1
+        if array[i] > maxim:
+            maxim = array[i]
+            frequency = 1
     return len(array) - frequency
-print(countGreater(list))
+
+
+print(countGreater(A))
--- perfect_number.py
+++ perfect_number.py
@@ -1,16 +1,18 @@
 # You are given an integer A. You have to tell whether it is a perfect number or not.
 
 import math
+
+
 def solve(A):
-        summ = 0
-        sqr = int(math.sqrt(A))
-        for i in range(1,sqr+1):
-            if A%i == 0:
-                if A/i ==i:
-                    summ +=i
-                else:
-                    summ = summ + i +A/i
-        if summ == 2*A:
-            return 1
-        else:
-            return 0
+    summ = 0
+    sqr = int(math.sqrt(A))
+    for i in range(1, sqr+1):
+        if A % i == 0:
+            if A/i == i:
+                summ += i
+            else:
+                summ = summ + i + A/i
+    if summ == 2 * A:
+        return 1
+    else:
+        return 0
