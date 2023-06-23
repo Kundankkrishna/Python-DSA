c695925c097df3ed42cb0ea4ea191f0e1d73aa47
Kundan Krishna <kundan.krishna@kyndryl.com>
2023-06-23 11:10:51 +0530

    enforced PEP8 to the file

--- count_of_primes.py
+++ count_of_primes.py
@@ -1,20 +1,22 @@
-#count the number of prime numbers less than a given number A
-
-num = int(input("Enter a number: "))
+# count the number of prime numbers less than a given number A
 import math
+num = int(input("Enter a number: "))
+
+
 def countprimes(A):
-    primecount =0
-    for n in range(1,A):
+    primecount = 0
+    for n in range(1, A):
         sqr = int(math.sqrt(n))
         factcount = 0
-        for i in range(1,sqr+1):
-            if n%i ==0:
+        for i in range(1, sqr+1):
+            if n % i == 0:
                 if n/i == i:
-                    factcount +=1
+                    factcount += 1
                 else:
-                    factcount +=2
-        if factcount ==2:
-            primecount +=1
+                    factcount += 2
+        if factcount == 2:
+            primecount += 1
     return primecount
+
 
 print(countprimes(num))     # prints the count of prime numbers less than num.
